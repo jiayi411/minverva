43c82493c45134bd7ff1771011b7eca30814b0cc
jiayi <jiayi411@qq.com>
2017-02-05 14:12:40 -0600

    thread-safe reference obj

--- foundation/foundation/basic/reference_object.h
+++ foundation/foundation/basic/reference_object.h
@@ -11,6 +11,7 @@
 #define reference_object_H
 
 #include "memory/allocator.h"
+#include <atomic>
 
 namespace minerva { namespace foundation {
     
@@ -18,11 +19,14 @@ namespace minerva { namespace foundation {
     /// @brief The basic class who need to count the reference
     ///
     /// This class is often used by smart_pointer to caculate
-    /// the reference count
+    /// the reference count. The operations are thread-safe
     ///
     class reference_object : public allocator
     {
     public:
+        /// ctor
+        reference_object() : _reference(0), _reference_lock(false) {}
+        
         /// dctor
         virtual ~reference_object() { _reference = 0; }
         
@@ -33,10 +37,11 @@ namespace minerva { namespace foundation {
         /// decrease the reference count
         inline void decrease_reference()
         {
-            mi_assert( _reference );
             --_reference;
+            while (_reference_lock.test_and_set());
             if (_reference == 0)
             { mi_delete this; }
+            _reference_lock.clear();
         }
         
         /// return the reference count
@@ -45,7 +50,8 @@ namespace minerva { namespace foundation {
         inline uint reference_count() const { return _reference; }
         
     protected:
-        uint _reference = 0;    ///< reference count
+        std::atomic_uint _reference;        ///< reference count
+        std::atomic_flag _reference_lock;   ///< lock
     };
 } }
 
